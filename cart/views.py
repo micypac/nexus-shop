09a6113ba9f1a120735ace16239eafaf289d9095
Michael Pacleb <michael.pacleb@gmail.com>
2023-07-06 11:06:21 -0400

    built a recommendation engine using Redis to recommend products that are usually bought together.

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
+from shop.recommender import Recommender
 
 
 @require_POST
@@ -37,12 +38,23 @@ def cart_detail(req):
             initial={"quantity": item["quantity"], "override": True}
         )
 
+    # display coupon form
     coupon_apply_form = CouponApplyForm()
+
+    # get items recommendations
+    r = Recommender()
+    cart_prods = [item["product"] for item in cart]
+    if cart_prods:
+        recommended_prods = r.suggest_products_for(cart_prods, max_results=4)
+    else:
+        recommended_prods = []
+
     return render(
         req,
         "cart/detail.html",
         {
             "cart": cart,
             "coupon_apply_form": coupon_apply_form,
+            "recommended_prods": recommended_prods,
         },
     )
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .recommender import Recommender
 
 
 def product_list(req, category_slug=None):
@@ -27,8 +28,16 @@ def product_detail(req, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
 
+    # get recommended products
+    r = Recommender()
+    recommended_prods = r.suggest_products_for([product], 4)
+
     return render(
         req,
         "shop/products/detail.html",
-        {"product": product, "cart_product_form": cart_product_form},
+        {
+            "product": product,
+            "cart_product_form": cart_product_form,
+            "recommended_prods": recommended_prods,
+        },
     )
